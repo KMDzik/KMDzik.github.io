da430906a719fb306a19c26721c69b2edb014f0c
KMDzik <katarzyna.dzik90@gmail.com>
2018-12-01 16:05:31 +0100

    mobile mQ start

--- slider.js
+++
@@ -1,7 +0,0 @@
-// (function () {
-//     setInterval(function () {
-//         var container = document.querySelector('.slider');
-//         var img = container.querySelector('img:first-child');
-//         container.appendChild(img);
-//     }, 7000)
-// })()
